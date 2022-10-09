f565f7a404a49448d2b8b01d40f91d1f7d39a078
Pavel Horal <pavel.horal@orchitech.cz>
2022-10-09 11:18:51 +0200

    Make the test simpler and fail faster.

--- duplexify.spec.js
+++ duplexify.spec.js
@@ -3,30 +3,27 @@ import { Duplex, PassThrough, Readable, Transform } from 'node:stream';
 describe('Duplex.from({ writable, readable })', () => {
 
   it('flushes stream after filling buffer', async () => {
-    // Create 16 items so that we overflow stream buffer
-    const values = [...new Array(16).keys()];
-
     // Simple pass-through as a placeholder for more complex setup
     const through = new PassThrough({ objectMode: true });
 
-    // Stream prepared values, pipe through simple duplex and async transformer
-    const stream = Readable.from(values, { objectMode: true })
+    // Stream prepared values, pipe through simple duplex and async transformer for backpressure
+    const stream = Readable.from(['foo', 'bar'], { objectMode: true })
       .pipe(Duplex.from({
         writable: through,
         readable: through
       }))
       .pipe(new Transform({
         objectMode: true,
+        highWaterMark: 1, // Setting 1 to force backpressure after a single item
         transform(chunk, encoding, callback) {
-          // We need to have async transformer to make sure buffer capacity is reached
           setTimeout(() => callback(null, chunk), 0);
         }
       }));
 
-    // This never finishes for 15+ items
+    // This never finishes when high water mark is reached
     const result = await stream.toArray();
 
-    expect(result).toEqual(values);
+    expect(result).toEqual(['foo', 'bar']);
   });
 
 });
